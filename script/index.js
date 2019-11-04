f85df8d7f2f33e55e0849bc4b5b63dd10c5b7d10
charon616 <milkyway.k16@gmail.com>
2019-11-04 14:10:14 +0900

    no message

--- script/index.js
+++ script/index.js
@@ -1,4 +1,7 @@
-AFRAME.registerComponent('artgalleryframe', {
+// マーカー画像の読み込み
+XR.XrController.configure({ imageTargets: ['dandelion', 'touch'] })
+
+AFRAME.registerComponent('workdetail', {
   schema: {
     name: { type: 'string' },
     rotated: { type: 'bool' },
@@ -14,7 +17,7 @@ AFRAME.registerComponent('artgalleryframe', {
     object3D.visible = false
 
     // Metadata comes to the primitive as a string, so we parse and destructure it
-    const { artist, date, title, wikiTitle } = JSON.parse(this.data.metadata)
+    const { title, artist, caption } = JSON.parse(this.data.metadata)
 
     const frameEl = document.createElement('a-entity')
     frameEl.setAttribute('scale', '0.95 0.95 0.95')
@@ -27,20 +30,21 @@ AFRAME.registerComponent('artgalleryframe', {
 
     // Instantiate the element with information about the painting
     const infoDisplay = document.createElement('a-entity')
-    infoDisplay.setAttribute('info-display', { title, artist, date })
+    infoDisplay.setAttribute('info-display', { title, artist })
     infoDisplay.object3D.position.set(0, this.data.rotated ? -0.4 : -0.5, 0.1)
     this.el.appendChild(infoDisplay)
 
     // Use the title of the painting to fetch some info from the Wikipedia API
     // If a painting doesn't have a Wikipedia article of its own, we use the painter's article via wikiTitle
-    const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&titles=${wikiTitle || title}&format=json&prop=extracts&exintro=1&origin=*`
-    let pageContent
-    fetch(apiUrl, { mode: 'cors' })
-      .then(e => e.json())
-      .then(data => {
-        const page = Object.entries(data.query.pages)[0][1]
-        pageContent = `<h1>${page.title}</h1>${page.extract}`
-      })
+    // const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&titles=${wikiTitle || title}&format=json&prop=extracts&exintro=1&origin=*`
+    // let pageContent
+    // fetch(apiUrl, { mode: 'cors' })
+    //   .then(e => e.json())
+    //   .then(data => {
+    //     const page = Object.entries(data.query.pages)[0][1]
+    //     pageContent = `<h1>${page.title}</h1>${page.extract}`
+    //   })
+    let pageContent = `<h1>${title}</h1>${caption}`
 
     const tapTarget = document.createElement('a-box')
     // Image targets are 3:4 so the target is scaled to match
@@ -89,12 +93,11 @@ AFRAME.registerComponent('info-display', {
   schema: {
     title: { default: '' },
     artist: { default: '' },
-    date: { default: '' },
   },
   init: function () {
     // Limit title to 20 characters
     const displayTitle = this.data.title.length > 20 ? `${this.data.title.substring(0, 17)}...` : this.data.title
-    const text = displayTitle + '\n' + this.data.artist + ', ' + this.data.date
+    const text = displayTitle + '\n' + this.data.artist
     const textData = {
       align: 'left',
       width: 0.7,
@@ -115,17 +118,14 @@ AFRAME.registerComponent('info-display', {
 })
 
 // xrextras-generate-image-targets uses this primitive to automatically populate multiple image targets
-AFRAME.registerPrimitive('test-frame', {
+AFRAME.registerPrimitive('work-detail', {
   defaultComponents: {
     artgalleryframe: {},
   },
 
   mappings: {
-    name: 'artgalleryframe.name',
-    rotated: 'artgalleryframe.rotated',
-    metadata: 'artgalleryframe.metadata',
+    name: 'workdetail.name',
+    rotated: 'workdetail.rotated',
+    metadata: 'workdetail.metadata',
   }
 })
-
-// マーカー画像の読み込み
-XR.XrController.configure({ imageTargets: ['dandelion', 'touch'] })
