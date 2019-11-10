49642b863b5a5e3850fe9cf0513bf3a40e5f12dc
charon616 <milkyway.k16@gmail.com>
2019-11-10 21:02:21 +0900

    loading

---
+++ script/load.js
@@ -0,0 +1,9 @@
+// AFRAME.registerComponent('hide-after-load', {
+//     init: function () {
+//         let scene = this.el.sceneEl
+//         let loadEl = document.querySelector("#loading");
+//         scene.addEventListener('realityready', () => {
+//             loadEl.style.display = "none"
+//         })
+//     }
+// });
