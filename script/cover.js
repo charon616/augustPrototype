a7c9297891abeb23376720d95e991868e9995b6b
charon616 <milkyway.k16@gmail.com>
2019-11-12 06:34:51 +0900

    fix

--- script/cover.js
+++ script/cover.js
@@ -18,11 +18,13 @@ AFRAME.registerComponent('close-button', {
     init: function () {
         const targetObj1 = document.getElementById('cover-scene')
         const targetObj2 = document.getElementById('venue')
+        const targetObj3 = document.getElementById('works')
         const closeButton = document.getElementById('closebutton')
 
         closeButton.onclick = () => {
             targetObj1.object3D.visible = false
             targetObj2.object3D.visible = false
+            targetObj3.object3D.visible = false
             closeButton.style.display = 'none'
         }
 
@@ -128,6 +130,36 @@ AFRAME.registerComponent('show-object', {
             logo.removeAttribute('mixin');
             button.style.display = 'none'
             object3D.visible = false
+        }
+
+        this.el.sceneEl.addEventListener('xrimagefound', showImage)
+        this.el.sceneEl.addEventListener('xrimageupdated', showImage)
+    }
+})
+
+AFRAME.registerComponent('show-object-orig', {
+    schema: {
+        name: {
+            type: 'string'
+        }
+    },
+    init: function () {
+        const object3D = this.el.object3D
+        const name = this.data.name
+        const button = document.getElementById('closebutton')
+        button.style.display = 'none'
+        object3D.visible = false
+        const logo = document.getElementById("logo-set");
+
+        const showImage = ({detail}) => {
+            if (name != detail.name) {
+                return
+            }
+            object3D.position.copy(detail.position)
+            // object3D.quaternion.copy(detail.rotation)
+            object3D.scale.set(detail.scale, detail.scale, detail.scale)
+            button.style.display = 'block'
+            object3D.visible = true
         }
 
         this.el.sceneEl.addEventListener('xrimagefound', showImage)
