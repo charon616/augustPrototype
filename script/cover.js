d11da8f412a4e8da81ed9c43478069e2ea81b03c
charon616 <milkyway.k16@gmail.com>
2019-11-14 07:17:58 +0900

    change script

--- script/cover.js
+++ script/cover.js
@@ -99,6 +99,53 @@ AFRAME.registerComponent('show-logo', {
     }
 })
 
+AFRAME.registerComponent('show-object', {
+    schema: {
+        name: {
+            type: 'string'
+        }
+    },
+    init: function () {
+        const object3D = this.el.object3D
+        const name = this.data.name
+        const button = document.getElementById('closebutton')
+        button.style.display = 'none'
+        object3D.visible = false
+        const logo = document.getElementById("logo-set");
+
+        const showImage = ({
+            detail
+        }) => {
+            if (name != detail.name) {
+                return
+            }
+            logo.setAttribute('mixin', 'appearAnimation')
+
+            object3D.position.copy(detail.position)
+            object3D.quaternion.copy(detail.rotation)
+            object3D.scale.set(detail.scale, detail.scale, detail.scale)
+
+            button.style.display = 'block'
+            object3D.visible = true
+        }
+
+        const hideImage = ({
+            detail
+        }) => {
+            if (name != detail.name) {
+                return
+            }
+            logo.removeAttribute('mixin');
+            button.style.display = 'none'
+            object3D.visible = false
+        }
+
+        this.el.sceneEl.addEventListener('xrimagefound', showImage)
+        this.el.sceneEl.addEventListener('xrimageupdated', showImage)
+        //this.el.sceneEl.addEventListener('xrimagelost', hideImage)
+    }
+})
+
 AFRAME.registerComponent('show-work', {
     schema: {
         name: { type: 'string' }
