8d11b9f985c9147958897fbe297d8f28931516e5
charon616 <milkyway.k16@gmail.com>
2019-11-14 09:33:17 +0900

    modosi

--- script/cover.js
+++ script/cover.js
@@ -182,3 +182,17 @@ AFRAME.registerComponent('show-caption', {
         })
     }
 })
+
+AFRAME.registerComponent('my-xrextras-one-finger-rotate', {
+    init: function () {
+        this.handleEvent = this.handleEvent.bind(this)
+        this.el.sceneEl.addEventListener('onefingermove', this.handleEvent)
+        this.el.setAttribute('class', 'cantap')
+    },
+    remove: function () {
+        this.el.sceneEl.removeEventListener('onefingermove', this.handleEvent)
+    },
+    handleEvent: function (event) {
+        this.el.object3D.rotation.y += event.detail.positionChange.x * 6
+    }
+})
