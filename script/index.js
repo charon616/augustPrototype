383da5356bf327f0fd96dd58c986e7f29860e4e0
charon616 <milkyway.k16@gmail.com>
2019-11-15 08:34:25 +0900

    色々軽量化

---
+++ script/index.js
@@ -0,0 +1,141 @@
+AFRAME.registerComponent('link-with-newwindow', {
+    schema: {
+        href: {
+            type: 'string'
+        }
+    },
+    init: function () {
+        this.el.addEventListener('click', (e) => {
+            setTimeout(() => {
+                if (window.open(this.data.href)) {} else {
+                    window.location.href = this.data.href;
+                };
+            }, 300);
+        })
+    }
+})
+
+const logo = document.getElementById("logo-set");
+
+AFRAME.registerComponent('close-button', {
+    init: function () {
+        const targetObj1 = document.getElementById('cover-scene')
+        const targetObj2 = document.getElementById('venue')
+        const targetObj3 = document.getElementById('works')
+        const closeButton = document.getElementById('closebutton')
+
+        closeButton.onclick = () => {
+            targetObj1.object3D.visible = false
+            targetObj2.object3D.visible = false
+            targetObj3.object3D.visible = false
+            closeButton.style.display = 'none'
+            logo.removeAttribute('mixin');
+        }
+    }
+})
+
+AFRAME.registerComponent('shadow-material', {
+    init: function () {
+        this.material = new THREE.ShadowMaterial()
+        this.el.getOrCreateObject3D('mesh').material = this.material
+        this.material.opacity = 0.3
+    }
+})
+
+AFRAME.registerComponent('show-caption', {
+    schema: {
+        num: {　type: 'int'　},
+        title: {　type: 'string'　},
+        name: {　type: 'string'　},
+        caption: {　type: 'string'　},
+    },
+    init: function () {
+        const contents = document.getElementById('contents')
+        const container = document.getElementById('container')
+        const tapTarget = this.el
+        let pageContent = `<h2>${this.data.title}</h2><h3>${this.data.name}</h3><p class="outer">${this.data.caption}</p>`
+
+        tapTarget.addEventListener('click', e => {
+            contents.innerHTML = pageContent
+            container.classList.remove('collapsed')
+        })
+    }
+})
+
+AFRAME.registerComponent('show-obj', {
+    schema: {
+        name: { type: 'string' },
+        content: { type: 'string' }
+    },
+    init: function () {
+        const object3D = this.el.object3D
+        const name = this.data.name
+        const button = document.getElementById('closebutton')
+        button.style.display = 'none'
+        object3D.visible = false
+
+        const showImage = ({ detail }) => {
+            if (name != detail.name) {
+                return
+            }
+            if(this.data.type　==　'logo'){
+                logo.setAttribute('mixin', 'appearAnimation')
+            }
+            object3D.position.copy(detail.position)
+            object3D.quaternion.copy(detail.rotation)
+            object3D.scale.set(detail.scale, detail.scale, detail.scale)
+            object3D.visible = true
+            button.style.display = 'block'
+        }
+        this.el.sceneEl.addEventListener('xrimagefound', showImage)
+        this.el.sceneEl.addEventListener('xrimageupdated', showImage)
+    }
+})
+
+AFRAME.registerComponent('show-map', {
+    schema: {
+        name: {
+            type: 'string'
+        }
+    },
+    init: function () {
+        const object3D = this.el.object3D
+        const name = this.data.name
+        const button = document.getElementById('closebutton')
+        button.style.display = 'none'
+        object3D.visible = false
+
+        const mapEl = document.createElement('a-entity')
+        mapEl.setAttribute('scale', '0.5 0.5 0.5')
+        mapEl.setAttribute('my-xrextras-one-finger-rotate')
+        setTimeout(() => {
+            mapEl.setAttribute('gltf-model', 'url(https://cdn.glitch.com/e2d1efa5-515f-4351-8464-aaad485734c5%2FvenueDraco.gltf?v=1573703760474)');
+        }, 10)
+
+        const showImage = ({
+            detail
+        }) => {
+            if (name != detail.name) {
+                return
+            }
+            this.el.appendChild(mapEl)
+            object3D.position.copy(detail.position)
+            object3D.quaternion.copy(detail.rotation)
+            button.style.display = 'block'
+            object3D.visible = true
+        }
+
+        const updateImage = ({
+            detail
+        }) => {
+            if (name != detail.name) {
+                return
+            }
+            object3D.position.copy(detail.position)
+            object3D.quaternion.copy(detail.rotation)
+        }
+
+        this.el.sceneEl.addEventListener('xrimagefound', showImage)
+        this.el.sceneEl.addEventListener('xrimageupdated', updateImage)
+    }
+})
