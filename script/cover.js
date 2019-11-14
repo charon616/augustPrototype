ea8dc5d8adf6a08d290cd712712555e10e914049
charon616 <milkyway.k16@gmail.com>
2019-11-14 10:22:39 +0900

    test

--- script/cover.js
+++ script/cover.js
@@ -78,8 +78,7 @@ AFRAME.registerComponent('show-work', {
         const arrow = document.getElementById('arrow')
         button.style.display = 'none'
         object3D.visible = false
-
-        const workplane = document.getElementById('work-plane')
+        arrow.objecrt3D.visible = false
 
         const showImage = ({ detail }) => {
             if (name != detail.name) { return }
@@ -98,31 +97,31 @@ AFRAME.registerComponent('show-work', {
     }
 })
 
-AFRAME.registerComponent('show-map', {
-    schema: {
-        name: { type: 'string' }
-    },
-    init: function () {
-        const object3D = this.el.object3D
-        const name = this.data.name
-        const button = document.getElementById('closebutton')
-        button.style.display = 'none'
-        object3D.visible = false
+// AFRAME.registerComponent('show-map', {
+//     schema: {
+//         name: { type: 'string' }
+//     },
+//     init: function () {
+//         const object3D = this.el.object3D
+//         const name = this.data.name
+//         const button = document.getElementById('closebutton')
+//         button.style.display = 'none'
+//         object3D.visible = false
 
-        const showImage = ({detail}) => {
-            if (name != detail.name) {
-                return
-            }
-            object3D.position.copy(detail.position)
-            object3D.quaternion.copy(detail.rotation)
-            button.style.display = 'block'
-            object3D.visible = true
-        }
+//         const showImage = ({detail}) => {
+//             if (name != detail.name) {
+//                 return
+//             }
+//             object3D.position.copy(detail.position)
+//             object3D.quaternion.copy(detail.rotation)
+//             button.style.display = 'block'
+//             object3D.visible = true
+//         }
 
-        this.el.sceneEl.addEventListener('xrimagefound', showImage)
-        this.el.sceneEl.addEventListener('xrimageupdated', showImage)
-    }
-})
+//         this.el.sceneEl.addEventListener('xrimagefound', showImage)
+//         this.el.sceneEl.addEventListener('xrimageupdated', showImage)
+//     }
+// })
 
 AFRAME.registerComponent('show-caption', {
     schema: {
