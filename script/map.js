8b7b020a34673c10d68de70833291e3ff3b3c21e
charon616 <milkyway.k16@gmail.com>
2019-11-14 13:20:10 +0900

    mapel

--- script/map.js
+++ script/map.js
@@ -11,19 +11,31 @@ AFRAME.registerComponent('show-map', {
         button.style.display = 'none'
         object3D.visible = false
 
+        const mapEl = document.createElement('a-entity')
+        mapEl.setAttribute('scale', '0.5 0.5 0.5')
+        mapEl.setAttribute('my-xrextras-one-finger-rotate')
+        mapEl.setAttribute('gltf-model', 'url(https://cdn.glitch.com/e2d1efa5-515f-4351-8464-aaad485734c5%2FvenueDraco.gltf?v=1573703760474)')
+
         const showImage = ({
             detail
         }) => {
             if (name != detail.name) {
                 return
             }
+            this.el.appendChild(mapEl)
             object3D.position.copy(detail.position)
             object3D.quaternion.copy(detail.rotation)
             button.style.display = 'block'
             object3D.visible = true
         }
 
+        const updateImage = ({ detail }) => {
+            if (name != detail.name) { return }
+            object3D.position.copy(detail.position)
+            object3D.quaternion.copy(detail.rotation)
+        }
+
         this.el.sceneEl.addEventListener('xrimagefound', showImage)
-        this.el.sceneEl.addEventListener('xrimageupdated', showImage)
+        this.el.sceneEl.addEventListener('xrimageupdated', updateImage)
     }
 })
