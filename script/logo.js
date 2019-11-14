271d539659c12e620ff2a63e0ffa75bd3bfd7ff2
charon616 <milkyway.k16@gmail.com>
2019-11-14 10:26:52 +0900

    split js file

--- script/logo.js
+++ script/logo.js
@@ -1,4 +1,4 @@
-AFRAME.registerComponent('show-map', {
+AFRAME.registerComponent('show-logo', {
     schema: {
         name: {
             type: 'string'
@@ -9,6 +9,7 @@ AFRAME.registerComponent('show-map', {
         const name = this.data.name
         const button = document.getElementById('closebutton')
         button.style.display = 'none'
+        const logo = document.getElementById("logo-set");
         object3D.visible = false
 
         const showImage = ({
@@ -17,12 +18,13 @@ AFRAME.registerComponent('show-map', {
             if (name != detail.name) {
                 return
             }
+            logo.setAttribute('mixin', 'appearAnimation')
             object3D.position.copy(detail.position)
             object3D.quaternion.copy(detail.rotation)
+            object3D.scale.set(detail.scale, detail.scale, detail.scale)
             button.style.display = 'block'
             object3D.visible = true
         }
-
         this.el.sceneEl.addEventListener('xrimagefound', showImage)
         this.el.sceneEl.addEventListener('xrimageupdated', showImage)
     }
