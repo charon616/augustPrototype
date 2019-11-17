9c0fafb687408916e7941536ff5e56ac743de878
charon616 <milkyway.k16@gmail.com>
2019-11-18 06:07:38 +0900

    shatter

--- script/index.js
+++ script/index.js
@@ -120,36 +120,36 @@ AFRAME.registerComponent('pinch-scale', {
     }
 })
 
-// AFRAME.registerComponent('photo-mode', {
-//     init: function () {
-//         const container = document.getElementById('photoModeContainer')
-//         const image = document.getElementById('photoModeImage')
-//         const shutterButton = document.getElementById('shutterButton')
-//         const returnButton = document.getElementById('returnButton')
-//         // Container starts hidden so it isn't visible when the page is still loading
-//         // container.style.display = 'block'
-//         returnButton.addEventListener('click', () => {
-//             container.classList.remove('photo')
-//         })
-//         shutterButton.addEventListener('click', () => {
-//             // Emit a screenshotrequest to the xrweb component
-//             this.el.sceneEl.emit('screenshotrequest')
+AFRAME.registerComponent('photo-mode', {
+    init: function () {
+        const container = document.getElementById('photoModeContainer')
+        const image = document.getElementById('photoModeImage')
+        const shutterButton = document.getElementById('shutterButton')
+        const returnButton = document.getElementById('returnButton')
+        // Container starts hidden so it isn't visible when the page is still loading
+        // container.style.display = 'block'
+        returnButton.addEventListener('click', () => {
+            container.classList.remove('photo')
+        })
+        shutterButton.addEventListener('click', () => {
+            // Emit a screenshotrequest to the xrweb component
+            this.el.sceneEl.emit('screenshotrequest')
 
-//             // Show the flash while the image is being taken
-//             container.classList.add('flash')
-//         })
-//         this.el.sceneEl.addEventListener('screenshotready', e => {
-//             // Hide the flash
-//             container.classList.remove('flash')
-//             // If an error occurs while trying to take the screenshot, e.detail will be empty.
-//             // We could either retry or return control to the user
-//             if (!e.detail) {
-//                 return
-//             }
-//             // e.detail is the base64 representation of the JPEG screenshot
-//             image.src = 'data:image/jpeg;base64,' + e.detail
-//             // Show the photo
-//             container.classList.add('photo')
-//         })
-//     }
-// })
+            // Show the flash while the image is being taken
+            container.classList.add('flash')
+        })
+        this.el.sceneEl.addEventListener('screenshotready', e => {
+            // Hide the flash
+            container.classList.remove('flash')
+            // If an error occurs while trying to take the screenshot, e.detail will be empty.
+            // We could either retry or return control to the user
+            if (!e.detail) {
+                return
+            }
+            // e.detail is the base64 representation of the JPEG screenshot
+            image.src = 'data:image/jpeg;base64,' + e.detail
+            // Show the photo
+            container.classList.add('photo')
+        })
+    }
+})
